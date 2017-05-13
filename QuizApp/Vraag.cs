4609612ebffc4ed65048fdd080005aa1913950f0
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2017-05-13 21:29:51 +0200

    Simple Quiz

---
+++ QuizApp/Vraag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+    class Vraag
+    {
+        public string Inhoud { get; set; }
+        public string CorrectAntwoord { get; set; }
+
+        private string gegevenAntwoord;
+
+        public virtual void StelVraag()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(Inhoud);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Antwoord: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            gegevenAntwoord = Console.ReadLine();
+            Console.ResetColor();
+        }
+
+        public bool Check()
+        {
+            if(gegevenAntwoord == CorrectAntwoord)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
